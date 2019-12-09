c532247d94d5fa8f6cb70fd9a06e28f068c91c69
johnebri <john.ebr@yahoo.com>
2019-12-09 18:33:18 +0100

    axios

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>About Page Yeah?</p>
+        </div>
+    )
+}
+
+export default Rainbow(About);
+
